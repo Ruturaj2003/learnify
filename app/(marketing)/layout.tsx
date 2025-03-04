a6e3f0e2c3a94e038fdf3f0bbb028e47f6d41804
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-03-04 19:09:08 +0530

    Setup Clerk

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="">
+      Marketing Layout
+      {children}
+    </div>
+  );
+};
+export default MarketingLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from 'next';
+import { type Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -23,30 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <meta name="apple-mobile-web-app-title" content="Learnify" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="manifest" href="/manifest.json" />
+        </head>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
