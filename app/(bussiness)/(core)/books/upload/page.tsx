a0ab8a9f59434ca565761c903205f94c2dd0c9d2
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-03-08 21:25:11 +0530

    Update Navbar title on upload page to reflect current functionality

--- app/(bussiness)/(core)/books/upload/page.tsx
+++ app/(bussiness)/(core)/books/upload/page.tsx
@@ -3,7 +3,7 @@ import Navbar from '../../../_components/Navbar';
 const page = () => {
   return (
     <div className="flex flex-col gap-y-2">
-      <Navbar title="Dashboard"></Navbar>
+      <Navbar title="Upload Book"></Navbar>
       <div className="a"></div>
     </div>
   );
