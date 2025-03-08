8b79ccea84c06629b63d7544dbf4c4a601b47db1
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-03-08 21:16:26 +0530

    Refactor BookCard component: update layout for better structure, enhance styling, and improve text readability

--- app/(bussiness)/(core)/_components/BookCard.tsx
+++ app/(bussiness)/(core)/_components/BookCard.tsx
@@ -2,26 +2,26 @@ import { Button } from '@/components/ui/button';
 
 const BookCard = () => {
   return (
-    <div className="flex h-48 bg-amber-100 rounded-md shadow-md overflow-hidden">
+    // Card Container
+    <div className="flex h-48 w-full bg-amber-100 rounded-md shadow-md overflow-hidden">
       {/* Book Image (40% Width) */}
       <div className="flex-shrink-0 w-2/5 h-full bg-amber-400"></div>
 
       {/* Book Data (60% Width) */}
-      <div className="flex-grow bg-teal-400/60 flex flex-col p-4">
+      <div className="flex flex-col justify-between flex-grow bg-teal-50 p-3">
         {/* Book Title */}
-        <h2 className="text-md font-semibold truncate">
+        <h2 className="text-md font-semibold truncate max-w-full">
           Introduction to Data Science
         </h2>
 
         {/* Book Description (Truncated) */}
-        <p className="text-xs text-gray-700 line-clamp-5">
+        <p className="text-sm text-gray-800 line-clamp-3">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sit at
-          velit dignissimos!, Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Quis sit at velit dignissimos!
+          velit dignissimos!
         </p>
 
         {/* Buttons */}
-        <div className="flex justify-between mt-auto">
+        <div className="flex justify-between gap-x-2">
           <Button size="sm" variant="outline">
             Read
           </Button>
