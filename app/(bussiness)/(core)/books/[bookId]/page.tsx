19adc549046acf93ca208189146dee610d49ed2b
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-03-19 13:05:18 +0530

    Add dropdown menu for learning mode selection in BookSectionListPage and create DropdownMenu component

--- app/(bussiness)/(core)/books/[bookId]/page.tsx
+++ app/(bussiness)/(core)/books/[bookId]/page.tsx
@@ -1,5 +1,14 @@
+'use client';
+
 import Navbar from '@/app/(bussiness)/_components/Navbar';
 import ChapterCard from './_components/ChapterCard';
+import { useGlobalStore } from '@/stores';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 
 const chapters = [
   { number: '1', name: 'Introduction to AI' },
@@ -26,20 +35,38 @@ const chapters = [
 ];
 
 const BookSectionListPage = () => {
+  const mode = useGlobalStore((state) => state.mode);
+  const setMode = useGlobalStore((state) => state.setMode);
   return (
     <div className="flex flex-col items-center gap-y-4">
       <Navbar title="Book Name ...." />
       <div className="p-2 border bg-gradient-to-r from-amber-400 to-rose-300 rounded-2xl w-[80%] text-2xl text-center">
         Learning mode:
-        <select
-          name="learningMode"
-          className="border border-white p-1 pl-2 rounded-xl ml-2"
-        >
-          <option value="quiz">Quiz</option>
-          <option value="read">Read</option>
-          <option value="explain">Explain</option>
-        </select>
+        {mode}
       </div>
+
+      {/* Learning Mode */}
+      <DropdownMenu>
+        <DropdownMenuTrigger className="text-2xl p-2 border bg-gradient-to-r from-amber-400 to-rose-300 rounded-2xl w-[80%]  text-center ">
+          Learning Mode : {mode}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="">
+          <DropdownMenuItem
+            onClick={() => setMode('explain')}
+            className="text-xl"
+          >
+            Explain
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setMode('read')} className="text-xl">
+            Read
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setMode('quiz')} className="text-xl">
+            {' '}
+            Quiz
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
       <div className="p-4 space-y-4 w-full max-w-2xl">
         {chapters.map((chapter) => (
           <ChapterCard
