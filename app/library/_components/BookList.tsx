739d6fcd994c2adb946c13d08f0b0894b3ee533b
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-04-28 12:55:05 +0530

    Make Book interface properties optional for better flexibility

--- app/library/_components/BookList.tsx
+++ app/library/_components/BookList.tsx
@@ -1,15 +1,15 @@
 import BookCard from './BookCard';
 
 export interface Book {
-  id: string; // MongoDB ObjectId as a string
-  userId: string; // The ID of the user associated with the book
+  id?: string; // MongoDB ObjectId as a string
+  userId?: string; // The ID of the user associated with the book
   title: string; // Title of the book
   category: string; // Category of the book
   description?: string; // Optional description of the book
-  fileUrl: string; // URL of the book file
+  fileUrl?: string; // URL of the book file
   startsFrom?: number; // Optional field indicating where the book starts from
-  createdAt: Date; // Date when the book was created
-  lastAccessed: Date; // Date when the book was last accessed
+  createdAt?: Date; // Date when the book was created
+  lastAccessed?: Date; // Date when the book was last accessed
 }
 
 const BookList = ({ books }: { books: Book[] }) => (
